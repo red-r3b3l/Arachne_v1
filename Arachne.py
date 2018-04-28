4a22ab239ee3b0ab9c2974756204a53c14ed3444
red-r3b3l <31393975+red-r3b3l@users.noreply.github.com>
2018-04-27 22:21:14 -0400

    Add files via upload

---
+++ Arachne.py
@@ -0,0 +1,156 @@
+
+#!/usr/bin/env python
+import os
+import socket
+import subprocess
+import sys
+from datetime import datetime
+
+
+# VARIABLES & TO-DO LIST
+
+#TO-DO List
+#
+# 1. Fix askforscantype menu function
+# 2. Add peripheral functions to SCSAF
+# 3. OS fingerprinting via Port 0 scans?
+# 4. SCSAF should check open ports against lists of known exploits and return suggestions!!!
+
+
+#setting up clearscreen function (which doesnt work :P  )
+clear = lambda: os.system('cls')
+clear()
+
+
+#set your scan type values here
+testscan = int("25"),int("110")
+quickscan = int("21"),int("22"),int("23"),int("25"),int("53"),int("443"),int("110"),int("135")
+fullscan = int("2"),int("3"),int("4"),int("5"),int("7"),int("8"),int("9"),int("10"),int("11"),int("12"),int("13"),int("15"),int("16"),int("17"),int("18"),int("19"),int("20"),int("21"),int("22"),int("23"),int("25"),int("27"),int("30"),int("31"),int("34"),int("37"),int("39"),int("41"),int("42"),int("43"),int("44"),int("48"),int("49"),int("50"),int("51"),int("52"),int("53"),int("54"),int("57"),int("58"),int("59"),int("66"),int("67"),int("68"),int("69"),int("70"),int("73"),int("77"),int("79"),int("80"),int("82"),int("85"),int("86"),int("87"),int("88"),int("90"),int("96"),int("97"),int("98"),int("99"),int("101"),int("102"),int("103"),int("105"),int("106"),int("107"),int("109")
+
+#creating empty list to store our open ports in
+openports = []
+vulns = []
+
+
+def mainmenu():
+    print("")
+    print("                      ARACHNE v1.00")
+    print("             Developed by MafiaSec Cybersecurity")
+    print("                    www.mafiasec.net")
+    print("")
+    print("1. Port Scan.")
+    print("2. Resolve Hostname")
+    print("3. DNS Reverse Lookup (Domain from IP)")
+    print("4. MX Record Lookup")
+    print("5. Check IP Location")
+    print("6. Curl URL")
+    print("7. Create Shell (One-Time)")
+    print("8. Create Shell (Scheduled)")
+    promptfor = input()
+    if promptfor == "1":
+        askforscantype()
+    if promptfor == "2":
+        vulnreport()
+
+
+def askforscantype():
+    print("What type of scan would you like to run?")
+    print("")
+    print("1. Quick Scan")
+    print("2. Full Scan")
+    print("")
+    print("Type the corresponding number and press enter:")
+    promptfor = input()
+    if promptfor == "1":
+        quickscanaskforhost()
+    elif promptfor == "2":
+        fullscanaskforhost()
+
+def quickscanaskforhost():
+    remoteServer = input("Enter a remote host to scan: ")
+    remoteServerIP = socket.gethostbyname(remoteServer)
+    print("-" * 75)
+    print("Please wait, Arachne is beginning quick test scan >>>>>", remoteServerIP)
+    print("-" * 75)
+    print("")
+    print("")
+    try:
+        for port in testscan:
+            sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+            result = sock.connect_ex((remoteServerIP, port))
+            if result == 0:
+                print("Port {}: $$$$$ Open $$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$".format(port))
+                openports.append(port)
+                #vulns.append(port)
+            else:
+                print("Port {}: 	 Closed".format(port))
+            sock.close()
+    except KeyboardInterrupt:
+        print("Exit.")
+        sys.exit()
+    except socket.gaierror:
+        print('Hostname could not be resolved.')
+        sys.exit()
+    except socket.error:
+        print("Couldn't connect to server. Socket error.")
+        sys.exit()
+    print("")
+    vulnreport()
+    mainmenu()
+
+def fullscanaskforhost():
+    remoteServer = input("Enter a remote host to scan: ")
+    remoteServerIP = socket.gethostbyname(remoteServer)
+    print("-" * 75)
+    print("Please wait, Arachne is beginning full port scan >>>>>", remoteServerIP)
+    print("-" * 75)
+    print("")
+    print("")
+    try:
+        for port in fullscan:
+            sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+            result = sock.connect_ex((remoteServerIP, port))
+            if result == 0:
+                print("Port {}: $$$$$ Open $$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$".format(port))
+                openports.append(port)
+            else:
+                print("Port {}: 	 Closed".format(port))
+            sock.close()
+    except KeyboardInterrupt:
+        print("Exit.")
+        sys.exit()
+    except socket.gaierror:
+        print('Hostname could not be resolved.')
+        sys.exit()
+    except socket.error:
+        print("Couldn't connect to server. Socket error.")
+        sys.exit()
+    print("")
+    print("Scan complete.")
+    print(vulns)
+
+def vulnreport():
+    if 25 in openports:
+        vulns.append("smtp_enum (Metasploit)")
+        vulns.append("smtp_version (Metasploit)")
+        vulns.append("Telnet E-mail Spoofing")
+    if 69 in openports:
+        vulns.append("TFTP Worm")
+    if 110 in openports:
+        vulns.append("pop3_something")
+    if 135 in openports:
+        vulns.append("Get Hostname from NetBIOS (nbname (Metasploit))")
+    if 445 in openports:
+        vulns.append("EternalBlue SMB Vulnerability")
+        vulns.append("SMB Based Exploits")
+    else:
+        print("None.")
+    print("Arachne believes the following vulnerabilities or exploits may be usable:")
+    print(vulns)
+
+
+
+mainmenu()
+
+
+
